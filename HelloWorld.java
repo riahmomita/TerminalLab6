0ac6487eb6276d2f47121567bb474b72244818c1
Momita Riah <riahmomita@yahoo.com>
2020-10-30 18:33:01 -0400

    # Description of the world in the month of October

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+	public static void main(String[]args){
+		System.out.println("Hello World");
+
+		#Adding an extra line to make a change in the program
+		System.out.println("The weather is really cold in October");
+	}
+}
